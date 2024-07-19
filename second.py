3879304967ec368d359522d60df2587ace7b05dc
Mitesh Poojari <miteshpoo@yahoo.com>
2024-07-19 21:56:58 +0100

    First files to git

---
+++ second.py
@@ -0,0 +1,25 @@
+x=1
+x=x+2
+print(x)
+if x < 10:
+    print('smaller')
+if x > 20: 
+    print ('bigger')
+n= 5
+while n > 0 :
+    print(n)
+    n=n-1
+
+
+print ('Finish')
+
+print(100.9982)
+
+print(type(100.9982))
+
+print(type('fdsfd'))
+
+name = input('Type')
+
+print(name)
+print(type(name))
